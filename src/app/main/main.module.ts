c742472c22f425e804e590e1c99e855a81c50479
feitosajoas <joasfeitosadelimasilva@gmail.com>
2021-12-23 17:50:59 -0300

    Começando a implementar a página inicial

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
+import { MainRoutingModule } from "./main-routing.module";
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [],
+  declarations: [HomeComponent],
+  imports: [CommonModule, MainRoutingModule],
 })
 export class MainModule {}
